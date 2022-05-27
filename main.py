c48240e3297e980e83130700d9a53477629dd1d2
Man Hin CHENG <chengman@ethz.ch>
2022-05-27 11:36:03 +0800

    first trial

--- main.py
+++ main.py
@@ -24,6 +24,12 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 moves = ['F', 'T', 'L', 'R']
 
+### 
+# User defined variables
+mo = 2
+###
+###
+
 @app.route("/", methods=['GET'])
 def index():
     return "Let the battle begin!"
@@ -32,7 +38,9 @@ def index():
 def move():
     request.get_data()
     logger.info(request.json)
-    return moves[random.randrange(len(moves))]
+    #return moves[random.randrange(len(moves))]
+    mo = 2 - mo
+    return moves[mo]
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
