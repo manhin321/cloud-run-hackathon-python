b68296d48187ea68564722ad7d5812e69749d7f8
Man Hin CHENG <chengman@ethz.ch>
2022-05-27 13:17:32 +0800

    keep throwing

--- main.py
+++ main.py
@@ -48,7 +48,8 @@ def move():
     else:
         return moves[1]
     """
-    return moves[random.randrange(len(moves))]
+    return moves[1]
+    #return moves[random.randrange(len(moves))]
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
